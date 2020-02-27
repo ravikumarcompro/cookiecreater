bb57d8befb53f9747e8ff9cb486633f9e5240712
ravikumarcompro <51909144+ravikumarcompro@users.noreply.github.com>
2020-02-27 11:15:52 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.post('/set-cookie', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.headers.origin ||req.get('origin') ||req.get('Origin') ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "*");
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Expose-Headers', 'Authorization, Cache-Control, Content-Length, Date, Expires, Server, Origin, Set-cookie');
+  res.header('Access-Control-Expose-Headers', 'Authorization, Cache-Control, Content-Length, Date, Expires, Server, Origin, Set-Cookie');
   let randomNumber=Math.random().toString();
   randomNumber=randomNumber.substring(2,randomNumber.length);
   res.cookie('mycokkieName',randomNumber, { domain: '.cokkie-validator.herokuapp.com', path: '/', httpOnly: true, secure: true, sameSite: false })
